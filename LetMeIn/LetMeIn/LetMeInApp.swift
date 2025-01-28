c8f060524f3ca3b6d93122f01b613c564a464359
Mark Kenneth Bayona <mkmbayona@gmail.com>
2025-01-28 22:53:18 +0800

    Create project

---
+++ LetMeIn/LetMeIn/LetMeInApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetMeInApp.swift
+//  LetMeIn
+//
+//  Created by Mark Kenneth Bayona on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct LetMeInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
